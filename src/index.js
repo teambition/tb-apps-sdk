4f7e94135211557b8a9dfc576f586f65fb6bb864
王艺霖 <yilin@teambition.com>
2016-07-15 03:03:55 +0800

    修复模块

--- src/index.js
+++ src/index.js
@@ -11,34 +11,34 @@ const consoleError = ({error}) => {
   return console.error(errorMessage);
 };
 
-export default TBApps = {
-  notify ({isCI, onError, onSuccess, params}) {
-    this.callService({
-      isCI,
-      params,
-      onSuccess,
-      onError,
-      method: 'essage'
-    });
-  },
-  callService ({isCI, method, params, onSuccess, onError}) {
-    const originPostId = Date.now();
-    const origin = isCI ? ORIGIN_CI : ORIGIN_PROD;
-    const content = {postId, method, params};
-    window.parent.postMessage(content, origin);
-    const callback = ({data: {postId, error}}) => {
-      if (postId !== originPostId) return
-      if (error) {
-        onError = onError || consoleError;
-        onError(data);
-      } else {
-        if (typeof onSuccess === "function") {
-          onSuccess(data);
-        }
-      }
-      toggleEventListener(callback, true);
-    };
+const notify = ({isCI, onError, onSuccess, params}) => {
+  callService({
+    isCI,
+    params,
+    onSuccess,
+    onError,
+    method: 'essage'
+  });
+};
 
-    toggleEventListener(callback);
-  }
+const callService = ({isCI, method, params, onSuccess, onError}) => {
+  const postId = Date.now();
+  const origin = isCI ? ORIGIN_CI : ORIGIN_PROD;
+  const content = {method, params, postId};
+  window.parent.postMessage(content, origin);
+  const callback = ({data}) => {
+    if (postId !== data.postId) return
+    if (data.error) {
+      onError = onError || consoleError;
+      onError(data);
+    } else {
+      if (typeof onSuccess === "function") {
+        onSuccess(data);
+      }
+    }
+    toggleEventListener(callback, true);
+  };
+  toggleEventListener(callback);
 }
+
+export { notify, callService };
